cd6679abd4e5c103cd9b0fa49d7714554862c5e3
manishsalian09 <manishsalian09@gmail.com>
2020-10-04 22:36:15 +0530

    updated

--- boyer_pattern_searching.py
+++ boyer_pattern_searching.py
@@ -4,15 +4,24 @@ class PatternSearching:
         self.text = text
 
     def search(self, pattern):
+        print(self.text)
+        print(pattern)
         index = -1
         t_index = len(pattern) - 1
         while t_index < len(self.text):
             diff_index = self.difference_index(pattern, t_index)
+            print("diff_index = " + str(diff_index) + " t_index = " + str(t_index))
             if diff_index == -1:
                 index = t_index - (len(pattern)-1)
                 break
             p_index = self.char_position_in_pattern(pattern, self.text[diff_index])
-            t_index =  diff_index + t_index - p_index 
+            print("p_index = " + str(p_index) + "char = " + str(pattern[p_index]))
+            if p_index == -1:
+                t_index = t_index + len(pattern)
+            else:
+                #t_index =  diff_index + t_index - p_index
+                t_index = t_index + p_index
+                print("incr pos = " + str(t_index))
         return index
 
     def difference_index(self, pattern, end_index):
@@ -27,10 +36,13 @@ class PatternSearching:
         return -1
 
     def char_position_in_pattern(self, pattern, c):
-        return pattern.rindex(c)
+        try:
+            return pattern.rindex(c)
+        except ValueError:
+            return -1
 
 
 pattern_searching = PatternSearching("dfManishddfad")
 
 print("index = " + str(pattern_searching.search("Manish")))
-print("index = " + str(pattern_searching.search("nis")))
+#print("index = " + str(pattern_searching.search("nis")))
